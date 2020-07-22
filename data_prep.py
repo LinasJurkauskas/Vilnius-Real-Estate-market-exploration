11a0ec9f72dc3fb70c4fe9f07b434f265f6ab756
Linas Jurkauskas <tiesioglinas@gmail.com>
2020-07-22 22:55:12 +0300

    test

--- data_prep.py
+++ data_prep.py
@@ -4,10 +4,8 @@ import pandas as pd
 def create_df_raw():
 	'''
 	Loads the raw data, makes the exclusion of unused data for analysis.
-	1) New apartments >= 2019 excluded.
-	2) extra large apartments > 120 sq. m excluded.
-	3) only apartments (no houses) are included (type = 1)
-	4) only furnished apartments are included (state = 2)
+	1) extra large apartments > 120 sq. m excluded.
+	2) only apartments (no houses) are included (type = 1)
 	'''
 	df_raw = pd.read_excel(r'C:\Users\tiesi\Class D\Vilnius-Real-Estate-market-exploration\Data.xlsx', index_col=0) 
 	#df_raw = df_raw[df_raw['year'] > 2018]
@@ -16,8 +14,6 @@ def create_df_raw():
 	df_raw['log_date'] = pd.to_datetime(df_raw['log_date'])
 	df_raw['week'] = df_raw['log_date'].dt.week
 	#df_raw = df_raw[df_raw['state']==2]
-
-
 
 	#select the columns for analysis
 	df_raw_columns = [ 'log_date', 'district', 'street', 'year', 'total_price',
@@ -37,6 +33,7 @@ def create_df(df_raw):
 	df = df_raw[0:1].copy()
 	df = df.reset_index()
 	df = df.drop(df.index[0])
+	df = df.drop(['ID'], axis=1)
 
 	#preparing the quantiles range dataframe
 	column_names = ['Q1','Q2','Q3','Q4','IQR', 
@@ -82,3 +79,33 @@ def create_df(df_raw):
 																}, name=(week)))
 
 	return df, quantiles_range
+
+
+
+def create_segments(df):
+	'''
+	Creates 3 data segmentations:
+	1) size segment
+	2) district segment
+	3) street segment
+	'''		
+
+	df['size_segment'] = pd.qcut(df['space_sq_m'], q=3, labels=["small", "medium", "large"])	
+
+	streets = df.groupby('street')['price_sq_m'].agg(['mean'])
+	streets['street_rank'] = pd.qcut(streets['mean'], q=5,  labels=[5, 4, 3,2,1])
+	df = pd.merge(df, streets['street_rank'], on='street', how='outer')	
+
+	districts = df.groupby('district')['price_sq_m'].agg(['mean'])
+	districts['district_rank'] = pd.qcut(districts['mean'], q=5,  labels=[5, 4, 3,2,1])
+	df = pd.merge(df, districts['district_rank'], on='district', how='outer')
+
+	#df['size_segment'] = df.size_segment.astype('category').cat.codes
+	#df['street_rank'] = df.street_rank.astype('category').cat.codes
+	#df['district_rank'] = df.district_rank.astype('category').cat.codes							
+
+	return df
+
+
+
+
